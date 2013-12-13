9a568f65827505177985b45ae47d5ad4d986119c
horho <h.horvat@gmail.com>
2013-12-13 13:55:16 +0100

    change web.js

--- web.js
+++ web.js
@@ -1,15 +1,58 @@
-var express = require('express');
+var http = require('http');
 var fs = require('fs');
+var util = require('util');
 
-var app = express.createServer(express.logger());
+var server = http.createServer();
+server.on('request', function(req, res) {
+	var file = "."+req.url;
+	if(file == "./") {
+		file = "./index.html";
+	}
+	var content = '';
+	var msg = 200;
+	fs.stat(file, function(err, stats) {
+		if(err) {
+			content = 'Error 404 '+err.message; 
+			msg = 404;
+			console.log('Error 404 '+err.message); 
+			return;
+		}
+		var readBuffer = new Buffer(stats.size);
 
-app.get('/', function(request, response) {
-	var content = fs.readFileSync("index.html").toString();
-
-  response.send(content);
+		fs.open(file, 'r', function(err, fd) {
+			if(err) {
+				content = 'Error 404: '+err.message;
+				msg = 404;
+				console.log('Error 404 '+err.message); 
+				return;
+			}
+			var bufferOffset = 0,
+					bufferLength = readBuffer.length,
+					filePosition = 0;
+			fs.read(fd, readBuffer, bufferOffset, bufferLength, filePosition, function(err, readBytes) {
+				if(err) {
+					content = 'Error 404: '+err.message;
+					msg = 404;
+					console.log('Error 404 '+err.message); 
+					return;
+				}
+				if(readBytes > 0) {
+					content = readBuffer.slice(0, readBytes).toString();
+				}
+			});
+		console.log(JSON.stringify(req.headers));
+		res.writeHead(200, {'Content-Type' : 'text/plain'});
+		res.write(content);
+		res.end();
+		});
+	});
 });
+server.listen(8080);
 
-var port = process.env.PORT || 8080;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+/*
+var util = require('util');
+require('http').createServer(function(req, res) {
+	res.writeHead(200, {'Content-Type' : 'text/plain'});
+	res.end(util.inspect(req.headers));
+}).listen(8080);
+*/
